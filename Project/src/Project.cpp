e1d3055d05d038a78bb64d831dfb52c9406a70e1
drlelis <drlelis@lin115-22.cise.ufl.edu>
2014-04-02 11:27:51 -0400

    setting up github for our project

---
+++ Project/src/Project.cpp
@@ -0,0 +1,15 @@
+/*
+ * Project.cpp
+ *
+ *  Created on: Apr 2, 2014
+ *      Author: drlelis
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<< "this is our project"<<endl;
+
+	return 0;
+}
