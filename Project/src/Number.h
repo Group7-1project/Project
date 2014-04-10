c27fea3c359f1e7b54df582a5263c0217ba86dc5
Marney <Marney@MyPC>
2014-04-10 18:44:22 -0400

    Number superclass

---
+++ Project/src/Number.h
@@ -0,0 +1,19 @@
+/*
+ * Number.h
+
+ *
+ *  Created on: Apr 7, 2014
+ *      Author: Marney
+ */
+
+#include <iostream>
+using namespace std;
+
+// our Number superclass
+class Number {
+
+public:
+	Number();
+	virtual ~Number();
+
+};
