d2feb7b60a8eab5d534ec37821e54d0814af1b13
CosmicCoder <lukekhouri@gmail.com>
2014-04-13 16:35:32 -0400

    Back!

--- Project/src/Exponential.cpp
+++ Project/src/Exponential.cpp
@@ -18,24 +18,24 @@ Number Exponential::simplify(int base, int power)  // All cases covered
 
 	if(base == 0 && power > 0) // 0^1
 	{
-		return 0;
+		return Fraction(0);
 	}
 
 	else if(base == 0 && power <= 0) // 0^-1
 	{
-		cout << "Not a valid operation." << endl; // Need to add exception handling
-		return 0;
+		throw invalid_argument("Dividing by zero"); // Warn calculator coder to
+													//catch exception
 	}
 
 	else if(base != 0 && power == 0) // 2^0
 	{
-		return 1;
+		return Fraction(1);
 	}
 
 	else if(base != 0 && power > 0) // 2^3
 	{
-		evaluate(base, power);
-		return ans; // here or there?
+		return evaluate(base, power);
+
 	}
 
 	else if(base != 0 && power < 0)  // 3^-2
@@ -49,53 +49,53 @@ Number Exponential::simplify(int base, int power)  // All cases covered
 Number Exponential::simplify(int base, Fraction power2) // All cases considered
 {
 
-	if(denominator == 0)    // 3^(1/0)
+	if(power2.denominator == 0)    // 3^(1/0)
 	{
-		return 0; 	// throw an error!
+		throw invalid_argument("Invalid exponent");
 	}
 
 	else if(base == 0 && power2 > 0)   // 0^(2/1)
 	{
-		return 0;
+		return Fraction(0);
 	}
 
 	else if(base == 0 && power2 <= 0)  // 0^0 or 0^(-6/1)
 	{
-		cout << "Not a valid operation." << endl; //Need to add exception handling
+		throw invalid_argument("Invalid entry");
 	}
 
 	else if(base !=0 && power2 == 0) // 3^(0/2)
 	{
-		return 1;
+		return Fraction(1);
 	}
 
-	else if(base < 0 && denominator%2 == 0)  // -3^(1/2)
+	else if(base < 0 && power2.denominator%2 == 0)  // -3^(1/2)
 	{
-		cout << "Cannot take even root of a negative number." << endl; //Need to add exception handling
-		return 0;
+		cout << "Cannot take even root of a negative number." << endl;
+		return Fraction(0);
 	}
 
-	else if(base > 0 && denominator%2 == 0 && power2 > 0) // 4^(3/2)
+	else if(base > 0 && power2.denominator%2 == 0 && power2 > 0) // 4^(3/2)
 	{
-		return evaluate2(sqrt(base), numerator); //Function pointers needed?; will ints work in sqrt and cbrt function?
+		return evaluate2(sqrt(base), power2.numerator); //Function pointers needed?; will ints work in sqrt and cbrt function?
 	}
 
-	else if(base > 0 && denominator%2 == 0 && power2 < 0) // 4^(-3/2)
+	else if(base > 0 && power2.denominator%2 == 0 && power2 < 0) // 4^(-3/2)
 	{
-		return 1/(evaluate2(sqrt(base), numerator*-1)); // parameter types...doubles and ints?
+		return 1/(evaluate2(sqrt(base), power2.numerator*-1));
 	}
 
-	else if(denominator%2 != 0 && numerator > 0) // 8^(2/3) and -8^(2/3)
+	else if(power2.denominator%2 != 0 && power2.numerator > 0) // 8^(2/3) and -8^(2/3)
 	{
-		return evaluate2(cbrt(base), numerator); //type check
+		return evaluate2(cbrt(base), power2.numerator); //type check
 	}
 
-	else if(denominator%2 != 0 && numerator < 0) // 8^(-2/3) and -8^(-2/3)
+	else if(power2.denominator%2 != 0 && power2.numerator < 0) // 8^(-2/3) and -8^(-2/3)
 		{
-			return 1/(evaluate2(cbrt(base), numerator)); // parameter compatibility..doubles and ints?
+			return 1/(evaluate2(cbrt(base), power2.numerator));
 		}
 
-	return 0;
+	return Fraction(0);
 }
 
 
@@ -110,20 +110,20 @@ Number Exponential::evaluate(int base, int power)
 
 	else
 	{
-		ans = (base * evaluate(base, power-1));
+		Number ans = (base * evaluate(base, power-1));
 		return ans;
 	}
 }
 
 Number Exponential::evaluate2(int base, Fraction power2)
 {
-	if(numerator == 1)
+	if(power2.numerator == 1)
 	{
 		return base;
 	}
 	else
 	{
-		ans2 = (base * evaluate2(base, numerator-1));
+		Number ans2 = (base * evaluate2(base, power2.numerator-1));
 		return ans2;
 	}
 
